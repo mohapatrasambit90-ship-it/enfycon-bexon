f9542328e389b6a8554293ff26c8dd3f67e07c19
imsahadeb <saha.ptc.om@gmail.com>
2026-01-20 14:39:58 +0530

    refactor: Update services page components and remove pricing plan, while adjusting slider styling for small screens.

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,10 +1,9 @@
-import Footer from "@/components/layout/footer/Footer";
+import Footer2 from "@/components/layout/footer/Footer2";
 import Header from "@/components/layout/header/Header";
-import Brands1 from "@/components/sections/brands/Brands1";
+import Brands2 from "@/components/sections/brands/Brands2";
 import Contact2 from "@/components/sections/contacts/Contact2";
 import Cta from "@/components/sections/cta/Cta";
 import HeroInner from "@/components/sections/hero/HeroInner";
-import PricingPlan from "@/components/sections/pricing-plan/PricingPlan";
 import ServicesPrimary from "@/components/sections/services/ServicesPrimary";
 import BackToTop from "@/components/shared/others/BackToTop";
 import HeaderSpace from "@/components/shared/others/HeaderSpace";
@@ -21,11 +20,10 @@ export default function Services() {
 						<HeroInner title={"Services"} text={"Services"} />
 						<ServicesPrimary />
 						<Contact2 />
-						<PricingPlan type={3} />
-						<Brands1 type={2} />
+						<Brands2 />
 						<Cta />
 					</main>
-					<Footer />
+					<Footer2/>
 				</div>
 			</div>
 
