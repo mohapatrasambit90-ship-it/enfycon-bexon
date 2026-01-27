e27bcda3993091b3339f816fa3addc30dcc7c832
imsahadeb <saha.ptc.om@gmail.com>
2026-01-27 23:53:44 +0530

    updte mobile menu

--- src/components/layout/header/MobileNavbar.js
+++ src/components/layout/header/MobileNavbar.js
@@ -7,12 +7,13 @@ import MobileMenuItem from "./MobileMenuItem";
 const MobileNavbar = () => {
 	const navItems = getNavItems();
 	const homeNav = navItems[0];
-	const pagesNav = navItems[1];
+	const aboutNav = navItems[1];
 	const serviceNav = navItems[2];
 	const industriesNav = navItems[3];
-	const blogsNav = navItems[4];
-	const aboutNav = navItems[5];
+	const productsNav = navItems[4];
+	const csrNav = navItems[5];
 	const contactNav = navItems[6];
+	const blogsNav = navItems[7];
 	return (
 		<div className="hamburger_menu">
 			<div className="mobile_menu mean-container">
@@ -35,161 +36,82 @@ const MobileNavbar = () => {
 									{homeNav?.name}
 								</Link>
 							</li>
-							<MobileMenuItem
-								text={pagesNav?.name}
-								url={pagesNav?.path}
-								submenuClass={"header__mega-menu mega-menu mega-menu-pages"}
-							>
-								<li>
-									<div className="mega-menu-wrapper">
-										{pagesNav?.submenu?.length
-											? pagesNav?.submenu?.map((pageItem, idx) => (
-												<div key={idx} className="mega-menu-pages-single">
-													<div className="mega-menu-pages-single-inner">
-														<h6 className="mega-menu-title">
-															{pageItem?.name}
-														</h6>
-														<div className="mega-menu-list">
-															{pageItem?.items?.length
-																? pageItem?.items?.map((item, idx2) => (
-																	<Link
-																		key={100 + idx2}
-																		href={item?.path ? item?.path : "/"}
-																		className={
-																			item?.isActive ? "active" : ""
-																		}
-																	>
-																		{item?.name}
-																		{item?.badge ? (
-																			<span
-																				className={`mega-menu-badge tj-zoom-in-out-anim ${item?.badge === "HOT"
-																					? "mega-menu-badge-hot"
-																					: ""
-																					}`}
-																			>
-																				{item?.badge}
-																			</span>
-																		) : (
-																			""
-																		)}
-																	</Link>
-																))
-																: ""}
-														</div>
-													</div>
-												</div>
-											))
-											: ""}
-										<div className="col-12 col-lg-3 mega-menu-pages-single">
-											<div className="mega-menu-pages-single-inner">
-												<div className="feature-box">
-													<div className="feature-content">
-														<h2 className="title">Modern</h2>
-														<span>Home Makeover</span>
-														<Link
-															className="read-more feature-contact"
-															href="tel:8321890640"
-														>
-															<i className="tji-phone-3"></i>
-															<span>+8 (321) 890-640</span>
-														</Link>
-													</div>
-													<div className="feature-images">
-														<Image
-															src="/images/service/service-ad.webp"
-															alt=""
-															width={370}
-															height={370}
-														/>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-								</li>
-							</MobileMenuItem>
-							<MobileMenuItem
-								text={serviceNav?.name}
-								url={serviceNav?.path ? serviceNav?.path : "#"}
-								submenuClass={"mega-menu-service"}
-							>
-								{serviceNav?.submenu?.length
-									? serviceNav?.submenu?.map((item, idx) => (
-										<li key={idx}>
-											<Link
-												className="mega-menu-service-single"
-												href={item?.path ? item?.path : "/"}
-											>
-												{" "}
-												<span className="mega-menu-service-icon">
-													<i
-														className={
-															item?.icon ? item?.icon : "tji-service-1"
-														}
-													></i>
-												</span>{" "}
-												<span className="mega-menu-service-title">
-													{item?.name
-														? item?.name
-														: "Business process optimization"}
-												</span>{" "}
-												<span className="mega-menu-service-nav">
-													<i className="tji-arrow-right-long"></i>
-													<i className="tji-arrow-right-long"></i>
-												</span>
-											</Link>
-										</li>
-									))
-									: ""}
-							</MobileMenuItem>
-							<MobileMenuItem
-								text={industriesNav?.name}
-								url={industriesNav?.path ? industriesNav?.path : "#"}
-								submenuClass={"mega-menu-service"}
-							>
-								{industriesNav?.submenu?.length
-									? industriesNav?.submenu?.map((item, idx) => (
-										<li key={idx}>
-											<Link
-												className="mega-menu-service-single"
-												href={item?.path ? item?.path : "/"}
-											>
-												{" "}
-												<span className="mega-menu-service-icon">
-													<i
-														className={
-															item?.icon ? item?.icon : "tji-industry"
-														}
-													></i>
-												</span>{" "}
-												<span className="mega-menu-service-title">
-													{item?.name
-														? item?.name
-														: "Industry"}
-												</span>{" "}
-												<span className="mega-menu-service-nav">
-													<i className="tji-arrow-right-long"></i>
-													<i className="tji-arrow-right-long"></i>
-												</span>
-											</Link>
-										</li>
-									))
-									: ""}
-							</MobileMenuItem>
-							<li>
-								<Link href={blogsNav?.path ? blogsNav?.path : "#"}>
-									{blogsNav?.name}
-								</Link>
-							</li>
 							<li>
 								<Link href={aboutNav?.path ? aboutNav?.path : "#"}>
 									{aboutNav?.name}
 								</Link>
 							</li>
+
+							<MobileMenuItem
+								text={serviceNav?.name}
+								url={serviceNav?.path ? serviceNav?.path : "#"}
+								submenuClass={"mega-menu-service"}
+							>
+								{serviceNav?.submenu?.map((category, idx) => (
+									<MobileMenuItem
+										key={idx}
+										text={category?.name}
+										url={category?.path}
+										submenuClass="mega-menu-service-nested"
+									>
+										{category?.items?.map((item, idx2) => (
+											<li key={idx2}>
+												<Link
+													href={item?.path ? item?.path : "/"}
+												>
+													{item?.name}
+												</Link>
+											</li>
+										))}
+									</MobileMenuItem>
+								))}
+							</MobileMenuItem>
+
+							<MobileMenuItem
+								text={industriesNav?.name}
+								url={industriesNav?.path ? industriesNav?.path : "#"}
+								submenuClass={"mega-menu-service"}
+							>
+								{industriesNav?.submenu?.map((item, idx) => (
+									<li key={idx}>
+										<Link
+											href={item?.path ? item?.path : "/"}
+										>
+											{item?.name}
+										</Link>
+									</li>
+								))}
+							</MobileMenuItem>
+
+							<MobileMenuItem
+								text={productsNav?.name}
+								url={productsNav?.path ? productsNav?.path : "#"}
+								submenuClass={"mega-menu-service"}
+							>
+								{productsNav?.submenu?.map((item, idx) => (
+									<li key={idx}>
+										<Link
+											href={item?.path ? item?.path : "/"}
+										>
+											{item?.name}
+										</Link>
+									</li>
+								))}
+							</MobileMenuItem>
+
+							<li>
+								<Link href={csrNav?.path ? csrNav?.path : "#"}>
+									{csrNav?.name}
+								</Link>
+							</li>
+							<li>
+								<Link href={blogsNav?.path ? blogsNav?.path : "#"}>
+									{blogsNav?.name}
+								</Link>
+							</li>
 							<li className="mean-last">
 								<Link href={contactNav?.path ? contactNav?.path : "#"}>
-									{" "}
-									{contactNav?.name ? contactNav?.name : "Contact"}
+									{contactNav?.name}
 								</Link>
 							</li>
 						</ul>
